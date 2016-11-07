7f4cdf22c6f430eba9a94956f6b2d2ddceb7e7a5
Olof Sjöholm <olof.sjoholm@tretton37.com>
2016-11-07 21:31:38 +0100

    Fixed not showing correct rotate card

--- core/src/olof/sjoholm/GameWorld/Client/Screens/RotateCardActor.java
+++ core/src/olof/sjoholm/GameWorld/Client/Screens/RotateCardActor.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import olof.sjoholm.GameWorld.Actors.Cards.BaseCard;
 import olof.sjoholm.GameWorld.Actors.Cards.RotateCard;
 import olof.sjoholm.GameWorld.Assets.Textures;
+import olof.sjoholm.GameWorld.Utils.Logger;
 import olof.sjoholm.GameWorld.Utils.Rotation;
 import olof.sjoholm.Interfaces.ICard;
 
@@ -14,13 +15,17 @@ public class RotateCardActor extends BaseCard {
     public RotateCardActor(ICard card) {
         super(card);
         Rotation rotation = ((RotateCard) card).getRotation();
+
         switch (rotation) {
             case LEFT:
                 texture = Textures.rotate_left;
+                break;
             case RIGHT:
                 texture = Textures.rotate_right;
+                break;
             case UTURN:
                 texture = Textures.rotate_uturn;
+                break;
         }
         build();
     }
