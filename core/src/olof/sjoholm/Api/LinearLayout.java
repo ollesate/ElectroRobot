c7db6c45d7963fcec08606909d7ae8eb6f9eba34
Olof Sjöholm <olof.sjoholm@thirdbase.se>
2017-08-30 20:28:00 +0200

    Fixed Linear layout

--- core/src/olof/sjoholm/Api/LinearLayout.java
+++ core/src/olof/sjoholm/Api/LinearLayout.java
@@ -9,23 +9,21 @@ public class LinearLayout extends Group {
     @Override
     public void addActor(Actor actor) {
         actor.setWidth(getWidth());
-        setHeight(getHeight() + actor.getHeight());
         super.addActor(actor);
     }
 
     @Override
-    public boolean removeActor(Actor actor) {
-        setHeight(getHeight() - actor.getHeight());
-        return super.removeActor(actor);
-    }
-
-    @Override
     protected void childrenChanged() {
-        float y = getHeight();
+        float totalHeight = 0;
+        for (Actor actor : getChildren()) {
+            totalHeight += actor.getHeight();
+        }
+        float y = totalHeight;
         for (Actor actor : getChildren()) {
             actor.setPosition(0, y, Align.topLeft);
             y -= actor.getHeight();
         }
+        setHeight(totalHeight);
     }
 
     @Override
