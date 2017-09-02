a27c411aac9935b9067cceda15f8dd6d448bb235
Olof Sjöholm <olof.sjoholm@thirdbase.se>
2017-09-02 09:42:40 +0200

    Clean envelope

--- core/src/olof/sjoholm/Net/Both/Envelope.java
+++ core/src/olof/sjoholm/Net/Both/Envelope.java
@@ -8,75 +8,16 @@ import java.util.List;
 import olof.sjoholm.Api.BoardAction;
 
 public class Envelope implements Serializable {
-    private Long responseId = -1L;
-
-    private final Object contents;
-    private int ownerId;
-
-    public Envelope() {
-        contents = null;
-    }
-
-    Envelope(final Object contents) {
-        this.contents = contents;
-    }
-
-    public <T> T getContents(Class<T> clazz) {
-        return clazz.cast(contents);
-    }
-
-    public Long getResponseId() {
-        return responseId;
-    }
-
-    public void tagWithResponseId(Long responseId) {
-        this.responseId = responseId;
-    }
 
     @Override
     public String toString() {
-        return "Envelope-> Type: " + getClass().getSimpleName() + ", Content: " + (contents != null ? contents.toString() : "null") + ", responseId " + responseId;
-    }
-
-    /**
-     * Package protected so this can not be changed.
-     */
-    void setOwnerId(int ownerId) {
-        this.ownerId = ownerId;
-    }
-
-    public int getOwnerId() {
-        return ownerId;
-    }
-
-    public static class Welcome extends Envelope {
-        public final int id;
-
-        public Welcome(int id) {
-            super(id);
-            this.id = id;
-        }
-    }
-
-    public static class ClientConnection extends Envelope {
-
-        public ClientConnection(NetClient netClient) {
-            super(netClient);
-        }
-    }
-
-    public static class ClientDisconnection extends Envelope {
-
-        public ClientDisconnection(NetClient netClient) {
-            super(netClient);
-        }
+        return "Envelope-> Type: " + getClass().getSimpleName() + ", Content: ";
     }
 
     public static class SendCards extends Envelope {
         public final List<BoardAction> cards;
 
         public SendCards(List<BoardAction> cards) {
-            super(cards);
             this.cards = cards;
         }
     }
