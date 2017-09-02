7be197a92454075970e156001046a7c843214d24
Olof Sjöholm <olof.sjoholm@thirdbase.se>
2017-09-02 10:49:26 +0200

    Deleted unused files

--- core/src/olof/sjoholm/Net/Both/Protocol.java
+++
@@ -1,8 +0,0 @@
-package olof.sjoholm.Net.Both;
-
-
-public class Protocol {
-
-    public static final String START_GAME = "START_GAME";
-
-}
--- core/src/olof/sjoholm/Net/ServerConstants.java
+++
@@ -1,9 +0,0 @@
-package olof.sjoholm.Net;
-
-
-public class ServerConstants {
-
-    public static final int CONNECTION_PORT = 9123;
-    public static final String HOST_NAME = "127.0.0.1";
-
-}
